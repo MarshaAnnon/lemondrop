2434243909f1c1e029bb2e1ac3ae6005831c7e7a
marsha <marshaannon@gmail.com>
2020-05-29 11:23:02 -0400

    fixed user association

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,16 +2,17 @@ class Book < ApplicationRecord
     has_many :book_genres
     has_many :genres, through: :book_genres
     has_many :quotes
-    has_many :users, through: :quotes
+    belongs_to :user
 
-    #accepts_nested_attributes_for :genres
+    accepts_nested_attributes_for :genres
+    accepts_nested_attributes_for :quotes
 
-    def genres_attributes=(genre_attributes)
-        genre_attributes.values.each do | genre_attribute |
-            if genre_attribute["name"].present?
-                genre = genre.find_or_create_by(genre_attribute)
-                self.categories << genre
-            end
-        end
-    end
+    #def genres_attributes=(genre_attributes)
+    #    genre_attributes.values.each do | genre_attribute |
+    #        if genre_attribute["name"].present?
+    #            genre = genre.find_or_create_by(genre_attribute)
+    #            self.categories << genre
+    #        end
+    #    end
+    #end
 end
