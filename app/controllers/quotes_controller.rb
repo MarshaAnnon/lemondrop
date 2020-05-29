73d2d1e13d75e6206df7a908eb1349c78d707d6e
marsha <marshaannon@gmail.com>
2020-05-29 11:49:37 -0400

    fix quote controller, add new, edit, update, and delete

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,16 +1,38 @@
 class QuotesController < ApplicationController
 
-    def create
-        quote = Quote.new(quote_params)
-        quote.save
-        redirect_to quote.post
-      end
-    
-      private
-    
-      def quote_params
-        params.require(:quote).permit(:content, :post_id, :user_id, user_attributes:[:username])
-      end
+  def new
+  end
+
+  def create
+    quote = Quote.new(quote_params)
+    quote.save
+    redirect_to book_path
+  end
+  
+  def update
+    if @quote
+      @quote.update(quote_params)
+        if @quote.errors.any?
+          render "edit"
+        else
+          redirect_to book_path
+        end
+    else
+      render "edit"
     end
+  end
+
+  def destroy
+      @quote.destroy
+      redirect_to root_path
+      flash[:notice] = "You have successfully deleted quote"
+  end
+  
+    private
+    
+    def quote_params
+      params.require(:quote).permit(:content, :user_id, :book_id)
+    end
+    
 
 end
