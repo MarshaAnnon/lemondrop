6243fdc2c37c3fbcdb3964f6929a0a865cc0faad
marsha <marshaannon@gmail.com>
2020-05-29 11:25:00 -0400

    fix association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :quotes
-  has_many :books, through: :quotes
+  has_many :books
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
