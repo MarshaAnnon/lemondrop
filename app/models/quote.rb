ed34078f43ad42a69d3bc74d932ee9d93522e894
marsha <marshaannon@gmail.com>
2020-05-29 11:24:26 -0400

    fix association

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,8 +2,9 @@ class Quote < ApplicationRecord
     belongs_to :user, optional: true
     belongs_to :book, optional: true
 
-  def user_attributes=(user_attributes)
-    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
-  end
+
+  #def user_attributes=(user_attributes)
+  #  self.user = User.find_or_create_by(username: user_attributes[:username]) unless #user_attributes[:username].blank?
+  #end
 
 end
