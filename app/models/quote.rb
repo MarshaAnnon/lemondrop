f8d17c71dfdb2f0861e70a1d726685b954174580
marsha <marshaannon@gmail.com>
2020-05-28 22:28:19 -0400

    add optional:true to quote.rb, create seed file

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ApplicationRecord
-    belongs_to :user
-    belongs_to :book
+    belongs_to :user, optional: true
+    belongs_to :book, optional: true
 
   def user_attributes=(user_attributes)
     self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
