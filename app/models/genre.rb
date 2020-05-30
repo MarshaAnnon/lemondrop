0fb6881618bcb1c1a362e3846099e6ec1799f5a2
marsha <marshaannon@gmail.com>
2020-05-30 18:20:40 -0400

    trying to work out some scope methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,10 @@
 class Genre < ApplicationRecord
     has_many :book_genres
     has_many :books, through: :book_genres
+
+    validates :name, presence: true
+
+    scope :genre, -> { where("books = '?'", title) }
+    
+    #scope method to return all the books associate with a genre
 end
