87bb915bc550dce8d2c1cf58f9c740e046551301
marsha <marshaannon@gmail.com>
2020-06-01 20:16:33 -0400

    working scope book_description

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,15 +8,15 @@ class Book < ApplicationRecord
     validates :author, presence: true
     
 
-    scope :author, -> { where("author = '?'", author) }
-    scope :description, -> { where(description: true) }
+    scope :author_name, -> { where("author = '?'", author) }
+    scope :book_description, -> { where("description > 0") }
     scope :book_quote, -> { where(quote: true) }
 
     #looking for a scope method that returns all the books associated with an author
     #looking for a scope method that returns the books with a description present
     #looking for a scope method that returns the books with a quote present
 
-    #accepts_nested_attributes_for :genres
+    accepts_nested_attributes_for :book_genres
     accepts_nested_attributes_for :quotes
 
     def genres_attributes=(genre_attributes)
